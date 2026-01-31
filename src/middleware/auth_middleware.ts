3a3556deb5c98f79daa62100847586b8b3a18c21
Tanvir <tanveer99ryan@gmail.com>
2026-01-31 21:40:53 +0600

    auth_middaleware add

---
+++ src/middleware/auth_middleware.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from "express";
+import { UserRole } from "../../generated/prisma/enums";
+
+import { auth as betterAuth } from "../lib/auth";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+
+const auth = (...role: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // get Session
+      const session = await betterAuth.api.getSession({
+        headers: req.headers as any,
+      });
+
+      if (!session) {
+        return res.status(403).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+
+      req.user = {
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified,
+      };
+
+      if (role.length && !role.includes(req.user.role as UserRole)) {
+        return res.status(400).json({
+          success: false,
+          message: "Forbidden",
+        });
+      }
+      next()
+    } catch (err) {}
+  };
+};
+
+export default auth;
--- src/modules/admin/admin.router.ts
+++ src/modules/admin/admin.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { adminController } from "./admin.controller";
+import auth from "../../middleware/auth_middleware";
+import { UserRole } from "../../enum/userRole";
 
-const router = Router()
+const router = Router();
 
-router.get("/users", adminController.getAllUser)
+router.get("/users", auth(UserRole.ADMIN), adminController.getAllUser);
 
-export const adminRouter = router
+export const adminRouter = router;
